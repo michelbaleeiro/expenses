2332bae15e0a20dc9b926b52f0078d12dda1e0df
Michel <michelgbaleeiro@gmail.com>
2022-11-14 21:05:55 -0300

    Aula 113. Fechando o Formulário (Modal)

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
-
 // ignore: use_key_in_widget_constructors
-class TransactionForm extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
+class TransactionForm extends StatefulWidget {
   final void Function(String, double) onSubmit;
 
-  TransactionForm(this.onSubmit, {Key? key}) : super(key: key);
+  const TransactionForm(this.onSubmit, {Key? key}) : super(key: key);
+
+  @override
+  State<TransactionForm> createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
 
   _submitForm() {
     final title = titleController.text;
@@ -17,7 +21,7 @@ class TransactionForm extends StatelessWidget {
       return;
     }
 
-    onSubmit(title, value);
+    widget.onSubmit(title, value);
   }
 
   @override
