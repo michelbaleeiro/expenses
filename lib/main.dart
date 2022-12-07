5f1518403e6d411cc2a7f92f21c232cdf407dec2
Michel <michelgbaleeiro@gmail.com>
2022-12-06 22:16:35 -0300

    144. Orientações Suportadas

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 import 'components/transaction_form.dart';
 import 'components/transaction_list.dart';
-import '../models/transaction.dart';
 import 'components/chart.dart';
+import '../models/transaction.dart';
 
 main() => runApp(const ExpensesApp());
 
